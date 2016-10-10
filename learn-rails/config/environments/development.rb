9fcac4af10f445a9ff222b5fed43c1664071a5be
Ireneusz Chojnacki <irek40@interia.pl>
2016-10-10 12:46:12 +0000

    add email configuration for mailings

--- learn-rails/config/environments/development.rb
+++ learn-rails/config/environments/development.rb
@@ -26,18 +26,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
   
   # ----------------------------------------------
-  # adds e-mail configuration from learnrails
+  # adds e-mail configuration from learn ruby on rails
   
   # sets up sendgrid mailer
   config.action_mailer.smtp_settings = {
@@ -57,6 +48,15 @@ Rails.application.configure do
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
   # -----------------------------------------------
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
